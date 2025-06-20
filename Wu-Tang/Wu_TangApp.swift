45ab34a9ceb81bdd30c6e2b06ecb2c4300dc487b
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-06-20 08:42:33 -0500

    Initial Commit

---
+++ Wu-Tang/Wu_TangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_TangApp.swift
+//  Wu-Tang
+//
+//  Created by Tushar Munge on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Wu_TangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
